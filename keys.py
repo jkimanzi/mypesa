f1ad0461dc4ac2a0bec9dff54abd956dd01e80be
joseph.kimanzi@gmail.com <joseph.kimanzi@gmail.com>
2019-11-01 16:13:55 +0300

    constants

--- keys.py
+++ keys.py
@@ -1,4 +1,4 @@
-business_code = "174379" # LNM shortcode
+business_code = "174379"
 passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
 consumer_key = "MBWASErTkup100iYGNGP6IbBoYmSwIvS"
 consumer_secret = "kG5AnYQBIZtpXSEm"
